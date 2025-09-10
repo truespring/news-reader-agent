a8ffa33386919c0ac85c6bf83706e4b71b2967e2
truespring <truespring1@gmail.com>
2025-09-10 16:52:41 +0900

    set to agents and tasks

--- main.py
+++ main.py
@@ -4,44 +4,52 @@ dotenv.load_dotenv()
 
 from crewai import Crew, Agent, Task
 from crewai.project import CrewBase, agent, task, crew
-from tools import count_letters
-
 
 @CrewBase
-class TranslatorCrew:
+class NewsReaderAgent:
 
     @agent
-    def translator_agent(self):
+    def news_hunter_agent(self):
         return Agent(
-            config=self.agents_config["translator_agent"],
+            config=self.agents_config["news_hunter_agent"]
         )
 
     @agent
-    def counter_agent(self):
+    def summarizer_agent(self):
         return Agent(
-            config=self.agents_config["counter_agent"],
-            tools=[count_letters],
+            config=self.agents_config["summarizer_agent"]
+        )
+
+    @agent
+    def curator_agent(self):
+        return Agent(
+            config=self.agents_config["curator_agent"]
         )
 
     @task
-    def translate_task(self):
+    def content_harvesting_task(self):
         return Task(
-            config=self.tasks_config["translate_task"],
+            config=self.tasks_config["content_harvesting_task"]
         )
 
     @task
-    def count_task(self):
+    def summarization_task(self):
         return Task(
-            config=self.tasks_config["count_task"],
+            config=self.tasks_config["summarization_task"]
+        )
+
+    @task
+    def final_report_assembly_task(self):
+        return Task(
+            config=self.tasks_config["final_report_assembly_task"]
         )
 
     @crew
-    def assemble_crew(self):
+    def crew(self):
         return Crew(
-            agents=self.agents,
             tasks=self.tasks,
-            verbose=True,
+            agents=self.agents,
+            verbose=True
         )
 
-
-TranslatorCrew().assemble_crew().kickoff(inputs={"sentence": "I'm Gyu and I like to ride my bicycle in Seoul", })
+NewsReaderAgent().crew().kickoff()
