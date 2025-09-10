399fe265dea4b2a77b37c9f0f9613c5879f1ea21
truespring <truespring1@gmail.com>
2025-09-10 15:36:58 +0900

    agent 첫 세팅해보기

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from news-reader-agent!")
+import dotenv
+
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
 
 
-if __name__ == "__main__":
-    main()
+@CrewBase
+class TranslatorCrew:
+
+    @agent
+    def translator_agent(self):
+        return Agent(
+            config=self.agents_config["translator_agent"],
+        )
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"],
+        )
+
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"],
+        )
+
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            verbose=True,
+        )
+
+
+TranslatorCrew().assemble_crew().kickoff(inputs={"sentence": "I'm Gyu and I like to ride my bicycle in Seoul", })
